d57e4d0ef2c81c877cfb0f7ec8a7f6335b16474f
Javier Garay <javier.garay@2brains.cl>
2020-08-27 17:43:48 -0400

    76. Configuración del Datasource de conexión a una base de datos MySQL - Práctica

--- src/main/java/com/jvrgaray/JpaDemoApplication.java
+++ src/main/java/com/jvrgaray/JpaDemoApplication.java
@@ -1,13 +1,23 @@
 package com.jvrgaray;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JpaDemoApplication {
+public class JpaDemoApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		guardar();
+	}
+	
+	private void guardar() {
+		System.out.println("Insertando un registro");
 	}
 
 }
