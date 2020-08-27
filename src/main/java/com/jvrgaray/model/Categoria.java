77ae40a1334c584e1e91fbe91216dd141391bfea
Javier Garay <javier.garay@2brains.cl>
2020-08-27 17:52:16 -0400

    78. Anotaciones JPA - @Entity, @Table, @Id, @GeneratedValue

--- src/main/java/com/jvrgaray/model/Categoria.java
+++ src/main/java/com/jvrgaray/model/Categoria.java
@@ -1,8 +1,16 @@
 package com.jvrgaray.model;
 
-import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="Categorias")
 public class Categoria {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nombre;
 	private String descripcion;
