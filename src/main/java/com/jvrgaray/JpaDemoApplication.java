f46c60a635023523acaf6c7ae6b5e11a411a63d8
Javier Garay <javier.garay@2brains.cl>
2020-08-28 12:19:14 -0400

    81. Operaciones CRUD - (Create) Persistir una entidad

--- src/main/java/com/jvrgaray/JpaDemoApplication.java
+++ src/main/java/com/jvrgaray/JpaDemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.jvrgaray.model.Categoria;
 import com.jvrgaray.repository.CategoriasRepository;
 
 @SpringBootApplication
@@ -18,11 +19,16 @@ public class JpaDemoApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println(repo);
+		guardar();
 	}
 	
 	private void guardar() {
-		System.out.println("Insertando un registro");
+		Categoria c = new Categoria();
+		c.setNombre("Finanzas");
+		c.setDescripcion("Trabajo relacionado con finanzas y contabilidad");
+		
+		repo.save(c);
+		System.out.println(c);
 	}
 	
 	private void eliminar() {
