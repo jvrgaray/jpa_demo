38e9a5fd7baf31a06910edcf2875edf95052976b
Javier Garay <javier.garay@2brains.cl>
2020-09-02 15:50:47 -0400

    98. Anotación @Transient de JPA

--- src/main/java/com/jvrgaray/model/Vacante.java
+++ src/main/java/com/jvrgaray/model/Vacante.java
@@ -7,6 +7,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
+
 
 @Entity
 @Table(name="Vacantes")
@@ -19,6 +21,11 @@ public class Vacante {
 	private Date fecha;
 	private Double salario;
 	private Integer destacado;
+	private String imagen="no-image.jpg";
+	private String estatus;
+	private String detalles;
+	@Transient
+	private Categoria categoria;
 	
 	public Integer getId() {
 		return id;
@@ -58,6 +65,68 @@ public class Vacante {
 		this.destacado = destacado;
 	}
 	
+	/**
+	* Obtiene el campo "image".
+	*
+	* @return image
+	*/
+	public String getImagen() {
+		return imagen;
+	}
+	/**
+	 * Modifica el campo "image" 
+	 * @param image image a modificar
+	 */
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+	/**
+	* Obtiene el campo "estatus".
+	*
+	* @return estatus
+	*/
+	public String getEstatus() {
+		return estatus;
+	}
+	/**
+	 * Modifica el campo "estatus" 
+	 * @param estatus estatus a modificar
+	 */
+	public void setEstatus(String estatus) {
+		this.estatus = estatus;
+	}
+	/**
+	* Obtiene el campo "detalles".
+	*
+	* @return detalles
+	*/
+	public String getDetalles() {
+		return detalles;
+	}
+	/**
+	 * Modifica el campo "detalles" 
+	 * @param detalles detalles a modificar
+	 */
+	public void setDetalles(String detalles) {
+		this.detalles = detalles;
+	}
+	/**
+	* Obtiene el campo "categoria".
+	*
+	* @return categoria
+	*/
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	/**
+	 * Modifica el campo "categoria" 
+	 * @param categoria categoria a modificar
+	 */
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
@@ -71,10 +140,17 @@ public class Vacante {
 		builder.append(fecha);
 		builder.append(", salario=");
 		builder.append(salario);
+		builder.append(", destacado=");
+		builder.append(destacado);
+		builder.append(", imagen=");
+		builder.append(imagen);
+		builder.append(", estatus=");
+		builder.append(estatus);
+		builder.append(", detalles=");
+		builder.append(detalles);
+		builder.append(", categoria=");
+		builder.append(categoria);
 		builder.append("]");
 		return builder.toString();
 	}
-	
-	
-	
 }
